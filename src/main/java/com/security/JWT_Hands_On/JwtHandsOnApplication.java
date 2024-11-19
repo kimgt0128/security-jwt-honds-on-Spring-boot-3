4f70517f403e236533aa679fefc1e50c1e892c84
KKT <kimgt2828@gmail.com>
2024-11-19 15:40:36 +0900

    setting: 프로젝트 생성 및 의존성 추가

---
+++ src/main/java/com/security/JWT_Hands_On/JwtHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.security.JWT_Hands_On;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtHandsOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/JWT_Hands_On/JwtHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.JWT_Hands_On;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
