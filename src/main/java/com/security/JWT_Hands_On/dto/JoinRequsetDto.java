69c451117017e32d69da148c51d8227937cd86ea
KKT <kimgt2828@gmail.com>
2024-11-21 01:52:52 +0900

    feat: 회원 가입 구현 - JoinController: 회원 가입 컨트롤러 - JoinService: 회원 중복 검사 및  가입 서비스 - MemberRepository: 회원 정보 관련 레포지토리

---
+++ src/main/java/com/security/JWT_Hands_On/dto/JoinRequsetDto.java
@@ -0,0 +1,14 @@
+package com.security.JWT_Hands_On.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class JoinRequsetDto {
+
+    private String memberName;
+    private String password;
+}
---
+++ src/main/java/com/security/JWT_Hands_On/service/JoinService.java
@@ -0,0 +1,36 @@
+package com.security.JWT_Hands_On.service;
+
+import com.security.JWT_Hands_On.dto.JoinRequsetDto;
+import com.security.JWT_Hands_On.entity.MemberJwt;
+import com.security.JWT_Hands_On.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+
+@Service
+public class JoinService {
+
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void joinProcess(JoinRequsetDto request) {
+
+        String memberName = request.getMemberName();
+        String password = request.getPassword();
+
+        //memberName 중복 검사
+        if (memberRepository.existsByMemberName(memberName)) {
+            System.out.println("이미 존재하는 회원입니다.");
+            return;
+        }
+
+        //회원 가입 진행
+        MemberJwt member = new MemberJwt();
+        member.setMemberName(memberName);
+        member.setMemberPassword(bCryptPasswordEncoder.encode(password));
+        member.setRole("ROLE_ADMIN");
+        memberRepository.save(member);
+    }
+}
