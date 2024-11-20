beb30d383b59d70ef82c3e854ff7628e079a5433
KKT <kimgt2828@gmail.com>
2024-11-20 23:54:57 +0900

    feat: DB연결, Entity 작성 - build.gradle: JPA, MySQL 의존성 추가 - MemberJwt: JWT 방식의 Member 엔티티 - MemberRepository: Member 레포지토리 - application.yml: 어플리케이션 설정 파일

--- src/main/java/com/security/JWT_Hands_On/securityConfig/SecurityConfig.java
+++ src/main/java/com/security/JWT_Hands_On/securityConfig/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
